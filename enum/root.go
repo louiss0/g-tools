1395cfdb3be2686d2a032ff5232c4733b0aa7494
Shelton Louis <louisshelton0@gmail.com>
2025-07-11 12:56:34 -0400

    docs(enum/package): add documentation and fix struct definition
    Added package-level comments and documentation for PureStringIntOrFloat, StringIntOrFloat interfaces, Enum type, and its methods.
    Corrected the Enum struct definition by removing a duplicate 'values' field, resolving a compilation error.

--- enum/root.go
+++ enum/root.go
@@ -1,3 +1,4 @@
+// Package enum provides a generic Enum type for defining and working with enumerations.
 package enum
 
 import (
@@ -8,22 +9,30 @@ type PureStringIntOrFloat interface {
 	string | int | float32 | float64
 }
 
+// StringIntOrFloat is a constraint that allows underlying types of string, int, float32, and float64.
 type StringIntOrFloat interface {
 	~string | ~int | ~float32 | ~float64
 }
 
+// Enum is a generic type that represents an enumeration of values.
+// T is the underlying type of the enumeration values.
+// U is the underlying type of the values used for parsing and validation.
 type Enum[T StringIntOrFloat, U PureStringIntOrFloat] struct {
 	values []T
 }
 
+// NewEnum creates a new Enum with the given values.
 func NewEnum[U PureStringIntOrFloat, T StringIntOrFloat](values ...T) Enum[T, U] {
 	return Enum[T, U]{values: values}
 }
 
+// Options returns the list of valid options for the Enum.
 func (e Enum[T, U]) Options() []T {
 	return e.values
 }
 
+// Parse checks if the given value is a valid option for the Enum.
+// It panics if the value is not valid.
 func (e Enum[T, U]) Parse(value U) {
 
 	for _, v := range e.values {
@@ -34,6 +43,8 @@ func (e Enum[T, U]) Parse(value U) {
 	panic(fmt.Sprintf("invalid value %v it must be one of %v", value, e.values))
 }
 
+// Validate checks if the given value is a valid option for the Enum.
+// It returns true if the value is valid, false otherwise.
 func (e Enum[T, U]) Validate(value U) bool {
 
 	for _, v := range e.values {
